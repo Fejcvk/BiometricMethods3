abd581c84bd8d699b54ac9c198b2d2abfe18ca95
Fejcvk <tomaszfejcak@icloud.com>
2018-01-23 02:36:53 +0100

    Task done

--- BiometriaLab3/Form1.Designer.cs
+++ BiometriaLab3/Form1.Designer.cs
@@ -28,23 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 830);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(398, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Binarize photo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(27, 33);
+            this.pictureBox1.Image = global::BiometriaLab3.Properties.Resources.eye_iris;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(768, 498);
+            this.pictureBox1.Size = new System.Drawing.Size(1252, 812);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(416, 830);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(330, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Detect edge";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(752, 830);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(330, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Find pupil and Iris";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1273, 38);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(161, 43);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Reset";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1088, 831);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(330, 42);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Clear background";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1267, 919);
+            this.ClientSize = new System.Drawing.Size(1448, 1013);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +117,11 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- BiometriaLab3/Form1.cs
+++ BiometriaLab3/Form1.cs
@@ -16,5 +16,163 @@ namespace BiometriaLab3
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(pictureBox1.Image);
+            binarize(bitmap, 15);
+        }
+
+        //Everything which is white or black or skin should be white, iris should be black
+        private void binarize(Bitmap _bitmap, int tresholdLevel)
+        {
+            Bitmap bitmap = _bitmap;
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    var currRgb = bitmap.GetPixel(x, y);
+                    var avgRgb = (currRgb.R + currRgb.G + currRgb.B) / 3;
+                    var newRgb = Color.FromArgb(currRgb.A, avgRgb, avgRgb, avgRgb);
+                    Color rgbToSet = newRgb.R > tresholdLevel || newRgb.G > tresholdLevel || newRgb.B > tresholdLevel ?
+                        rgbToSet = Color.FromArgb(255, 255, 255) : rgbToSet = Color.FromArgb(0, 0, 0);
+                    bitmap.SetPixel(x, y, rgbToSet);
+                }
+            }
+            pictureBox1.Image = bitmap;
+        }
+
+        #region FindEdgesRegion
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var bitmap = new Bitmap(pictureBox1.Image);
+            var black = Color.FromArgb(0, 0, 0);
+            for (int y = 0; y < bitmap.Height - 1; y++)
+            {
+                for (int x = 0; x < bitmap.Width - 1; x++)
+                {
+                    var currRed = bitmap.GetPixel(x, y).R - bitmap.GetPixel(x + 1, y + 1).R;
+                    var currGreen = bitmap.GetPixel(x, y).G - bitmap.GetPixel(x + 1, y + 1).G;
+                    var currBlue = bitmap.GetPixel(x, y).B - bitmap.GetPixel(x + 1, y + 1).B;
+                    var nextRed = bitmap.GetPixel(x + 1, y).R - bitmap.GetPixel(x, y + 1).R;
+                    var nextGreen = bitmap.GetPixel(x + 1, y).R - bitmap.GetPixel(x, y + 1).R;
+                    var nextBlue = bitmap.GetPixel(x + 1, y).R - bitmap.GetPixel(x, y + 1).R;
+                    Color newColor = Color.FromArgb(
+                        Math.Abs(currRed) + Math.Abs(nextRed) >= 255 ? 255 : 0,
+
+                        Math.Abs(currGreen) + Math.Abs(nextGreen) >= 255 ? 255 : 0,
+
+                        Math.Abs(currBlue) + Math.Abs(nextBlue) >= 255 ? 255 : 0);
+                    bitmap.SetPixel(x, y, newColor);
+                }
+            }
+            System.Diagnostics.Debug.WriteLine("Done");
+            pictureBox1.Image = bitmap;
+        }
+        #endregion
+        #region Find pupil and iris
+        int Max = 0;
+        int radMax = 0;
+        int iMax = 0;
+        int jMax = 0;
+        int irisRadius = 0;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var bitmap = new Bitmap(pictureBox1.Image);
+            var blackColor = Color.FromArgb(0, 0, 0);
+
+            int[,,] coordinateAndRadius3dArray = new int[pictureBox1.Image.Width + 1, pictureBox1.Image.Height + 1, 90];
+            for (int i = 0; i < pictureBox1.Image.Width; i++)
+            {
+                for (int j = 0; j < pictureBox1.Image.Height; j++)
+                {
+                    if (bitmap.GetPixel(i, j) != blackColor)
+                    {
+                        for (int radius = 10; radius <= 60; radius++)
+                        {
+                            for (int angle = 0; angle <= 360; angle++)
+                            {
+                                int a = (int)(i - (radius * Math.Cos(angle)));
+                                int b = (int)(j - (radius * Math.Sin(angle)));
+                                if (a > pictureBox1.Image.Width || a < 0 || b >pictureBox1.Image.Height || b < 0)
+                                    break;
+                                coordinateAndRadius3dArray[a, b, radius]++;
+                                if (coordinateAndRadius3dArray[a, b, radius] >= Max)
+                                {
+                                    Max = coordinateAndRadius3dArray[a, b, radius];
+                                    radMax = radius;
+                                    iMax = a;
+                                    jMax = b;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                using (Pen b = new Pen(Color.Red))
+                {
+                    g.DrawEllipse(b, iMax - radMax, jMax - radMax, radMax * 2, radMax * 2);
+
+                }
+            }
+            pictureBox1.Image = bitmap;
+            bitmap = new Bitmap(BiometriaLab3.Properties.Resources.eye_iris);
+            int midX = iMax + radMax / 2;
+            int midY = jMax + radMax / 2;
+            irisRadius = radMax;
+            int x = midX, y = midY;
+            while(bitmap.GetPixel(x,y).R <= 140 && bitmap.GetPixel(x,y).G <= 140 && bitmap.GetPixel(x,y).B <= 140 && y < bitmap.Height -1)
+            {
+                y++;
+                irisRadius++;
+            }
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                using (Pen b = new Pen(Color.Red))
+                {
+                    Pen c = new Pen(Color.Green);
+                    g.DrawEllipse(c, iMax - radMax, jMax - radMax, radMax * 2, radMax * 2);
+                    g.DrawEllipse(b, iMax - irisRadius, jMax - irisRadius, irisRadius * 2, irisRadius * 2);
+                }
+            }
+            pictureBox1.Image = bitmap;
+            #endregion
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = BiometriaLab3.Properties.Resources.eye_iris;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var bitmap = new Bitmap(pictureBox1.Image);
+            for(int i = 0; i < bitmap.Width; i++)
+            {
+                for(int j = 0; j < bitmap.Height; j++)
+                {
+
+
+                    if (Math.Pow((i - iMax), 2) + Math.Pow((j - jMax), 2) >= Math.Pow(irisRadius, 2) || Math.Pow((i - iMax), 2) + Math.Pow((j - jMax), 2) <= Math.Pow(radMax, 2))
+                        bitmap.SetPixel(i, j, Color.White);
+                }
+            }
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+
+                    var color = bitmap.GetPixel(i, j);
+                    if(color.R>150)
+                        bitmap.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+                    if (color.R > 140 && color.G > 140 && color.B > 140)
+                        bitmap.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+                    if (color.R > color.B + 50 && color.R > color.G + 50)
+                        bitmap.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+                }
+            }
+            pictureBox1.Image = bitmap;
+        }
     }
 }
